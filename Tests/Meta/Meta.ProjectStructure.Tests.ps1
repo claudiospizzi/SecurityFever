779e7251ac8a5915cc9d6e94f4e854c21f1b61aa
Claudio Spizzi <claudio.spizzi@live.com>
2016-09-14 13:52:30 +0200

    Update comment

--- Tests/Meta/Meta.ProjectStructure.Tests.ps1
+++ Tests/Meta/Meta.ProjectStructure.Tests.ps1
@@ -1,6 +1,7 @@
 
-# Placeholder test file. This file will be replaced during test run with the
-# latest version from claudiospizzi/PowerShellModuleBase repository.
+# Placeholder file. This file will be replaced during every test run with the
+# latest version from claudiospizzi/PowerShellModuleBase repository. Any changes
+# to this file are ignored by git.
 
 Describe 'Meta Project Structure' {
 
