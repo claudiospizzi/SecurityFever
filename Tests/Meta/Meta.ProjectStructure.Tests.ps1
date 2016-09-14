eeb2786d3e0173c20bb46d7c2c6770b1f1ec6995
Claudio Spizzi <claudio.spizzi@live.com>
2016-09-14 13:49:34 +0200

    Add meta project structure placeholder

---
+++ Tests/Meta/Meta.ProjectStructure.Tests.ps1
@@ -0,0 +1,11 @@
+
+# Placeholder test file. This file will be replaced during test run with the
+# latest version from claudiospizzi/PowerShellModuleBase repository.
+
+Describe 'Meta Project Structure' {
+
+    It 'should be replaced with latest version' {
+
+        $false | Should Be $true
+    }
+}
