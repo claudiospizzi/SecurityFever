d0c8fecbc46cd8084d9b1c8789235b9819a78b71
Claudio Spizzi <claudio.spizzi@live.com>
2016-12-05 14:34:57 +0100

    Remove unneeded usings

--- Sources/SecurityFever/CredentialManager/CredentialEntry.cs
+++ Sources/SecurityFever/CredentialManager/CredentialEntry.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Management.Automation;
-using System.Runtime.InteropServices;
+﻿using System.Management.Automation;
 using System.Security;
 using System.Text.RegularExpressions;
 
