c9db23322fddfac5c3af22500d51d6f6f8a121d0
Claudio Spizzi <claudio.spizzi@live.com>
2016-12-08 14:28:58 +0100

    Remove file catalog creation

--- Scripts/build.ps1
+++ Scripts/build.ps1
@@ -49,10 +49,6 @@ $ModuleVersion = (Import-PowerShellDataFile -Path "$ProjectPath\Modules\$ModuleN
 
 Write-Verbose '** BUILD'
 
-# Create a file catalog for the module folder. The catalog file will contain
-# hashes for all files. This is used to validate the whole module content.
-# New-FileCatalog -Path "$ProjectPath\Modules\$ModuleName" -CatalogFilePath "$ProjectPath\Modules\$ModuleName\$ModuleName.cat" -CatalogVersion 2.0 -Verbose:$VerbosePreference | Out-Null
-
 # In case of PowerShell, creating a build means zipping the requried files.
 # Thanks to the project structure, all requried but no extra files are in
 # the modules folder.
