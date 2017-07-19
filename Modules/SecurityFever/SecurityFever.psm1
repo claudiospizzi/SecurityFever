ee5902d3f66a07b117cee448e4d5d53fd5169d79
Claudio Spizzi <claudio.spizzi@arcade.ch>
2017-07-19 23:27:49 +0200

    Update module debug file

--- Modules/SecurityFever/SecurityFever.psm1
+++ Modules/SecurityFever/SecurityFever.psm1
@@ -1,12 +1,15 @@
+<# ----------------- DEBUGGING ONLY -- REMOVED DURING BUILD ----------------- #>
 
-# Get and dot source all helper functions (private)
+# Get and dot source all helper functions (internal)
 Split-Path -Path $PSCommandPath |
-    Join-Path -ChildPath 'Helpers' |
+    Get-ChildItem -Filter 'Helpers' -Directory |
         Get-ChildItem -Include '*.ps1' -Exclude '*.Tests.*' -Recurse |
             ForEach-Object { . $_.FullName }
 
 # Get and dot source all external functions (public)
 Split-Path -Path $PSCommandPath |
-    Join-Path -ChildPath 'Functions' |
+    Get-ChildItem -Filter 'Functions' -Directory |
         Get-ChildItem -Include '*.ps1' -Exclude '*.Tests.*' -Recurse |
             ForEach-Object { . $_.FullName }
+
+<# -------------------------------------------------------------------------- #>
