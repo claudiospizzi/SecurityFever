ee6b008e208c18342943bde8d1bacc3132686a0e
Claudio Spizzi <claudio@spizzi.net>
2017-11-30 22:50:23 +0100

    Update debug psm1 part

--- Modules/SecurityFever/SecurityFever.psm1
+++ Modules/SecurityFever/SecurityFever.psm1
@@ -3,13 +3,13 @@
 # Get and dot source all helper functions (internal)
 Split-Path -Path $PSCommandPath |
     Get-ChildItem -Filter 'Helpers' -Directory |
-        Get-ChildItem -Include '*.ps1' -Exclude '*.Tests.*' -Recurse |
+        Get-ChildItem -Include '*.ps1' -Exclude '*.Tests.*' -File -Recurse |
             ForEach-Object { . $_.FullName }
 
 # Get and dot source all external functions (public)
 Split-Path -Path $PSCommandPath |
     Get-ChildItem -Filter 'Functions' -Directory |
-        Get-ChildItem -Include '*.ps1' -Exclude '*.Tests.*' -Recurse |
+        Get-ChildItem -Include '*.ps1' -Exclude '*.Tests.*' -File -Recurse |
             ForEach-Object { . $_.FullName }
 
 <# -------------------------------------------------------------------------- #>
