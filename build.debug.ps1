03b58460c9440b95110dc4283be93c026b0afa5b
Claudio Spizzi <claudio@spizzi.net>
2017-12-05 00:30:18 +0100

    Update default debug script

--- build.debug.ps1
+++ build.debug.ps1
@@ -3,10 +3,7 @@
 $Env:PSModulePath = "$PSScriptRoot\Modules;$Env:PSModulePath"
 
 # Import all modules for the debugging session
+Get-ChildItem -Path "$PSScriptRoot\Modules" -Directory | ForEach-Object { Remove-Module $_.BaseName -ErrorAction 'SilentlyContinue' }
 Get-ChildItem -Path "$PSScriptRoot\Modules" -Directory | ForEach-Object { Import-Module $_.BaseName }
-
-# Now, stop the debugger at this point
-Set-PSBreakpoint -Script "$PSScriptRoot\build.debug.ps1" -Line 10 | Out-Null
-Write-Debug 'Stop debugger'
 
 <# ------------------ PLACE DEBUG COMMANDS AFTER THIS LINE ------------------ #>
