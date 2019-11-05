7cf086627331e801b6bcda93ff862f88a9f68cde
Claudio Spizzi <claudio.spizzi@arcade.ch>
2019-11-06 00:52:21 +0100

    Remove old project

--- Sources/SecurityFever/SecurityFever/CredentialManager/CredentialPersist.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SecurityFever.CredentialManager
-{
-    public enum CredentialPersist : uint
-    {
-        Session      = 0x01,
-        LocalMachine = 0x02,
-        Enterprise   = 0x03
-    }
-}
