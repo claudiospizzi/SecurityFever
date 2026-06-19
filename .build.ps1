22df4cb934a16e41ef5a69ff85802b0853244347
Claudio Spizzi <claudio.spizzi@arcade.ch>
2026-06-19 23:01:57 +0200

    feat(build): update token callbacks to use Get-BuildSecret

--- .build.ps1
+++ .build.ps1
@@ -3,5 +3,5 @@
 . InvokeBuildHelperTasks
 
 # Build configuration
-$IBHConfig.RepositoryTask.TokenCallback = { Use-VaultSecureString -TargetName 'GitHub Token (claudiospizzi)' }
-$IBHConfig.GalleryTask.TokenCallback    = { Use-VaultSecureString -TargetName 'PowerShell Gallery Key (claudiospizzi)' }
+$IBHConfig.GalleryTask.TokenCallback    = { Get-BuildSecret -EnvironmentVariable 'PS_GALLERY_KEY' -CredentialManager 'PowerShell Gallery Key (claudiospizzi)' }
+$IBHConfig.RepositoryTask.TokenCallback = { Get-BuildSecret -EnvironmentVariable 'GITHUB_TOKEN' -CredentialManager 'GitHub Token (claudiospizzi)' }
